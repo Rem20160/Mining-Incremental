4cc34129233a4f37bcc1f9bbc75e734e4eb3bf43
Rem20160 <142751203+Rem20160@users.noreply.github.com>
2023-08-21 20:30:34 +0300

    Update gameLoop.js
    Just added , "scientific" on formats

--- Mining/js/gameLoop.js
+++ Mining/js/gameLoop.js
@@ -3,8 +3,8 @@ var mainGameLoop = window.setInterval(function() {
     game.lastTick = Date.now()
     game.ore += game.orePerClick * (diff / 1000)
     update("oremined", format(game.ore, "scientific") + " ores")
-    update("perclickupgrade", format(game.orePerClickAdd) + "+" + " ore" + " Cost: " + format(game.orePerClickCost, "scientific") + " ore")
+    update("perclickupgrade", format(game.orePerClickAdd, "scientific) + "+" + " ore" + " Cost: " + format(game.orePerClickCost, "scientific") + " ore")
     update("perclickupgrademultiplier", "2x Multiplier " + "Cost: " + format(game.orePerClickMultiplierCost, "scientific") + " ore")
-    update("oreminedpersec", format(game.orePerClick) + " ore per one second")
+    update("oreminedpersec", format(game.orePerClick, "scientific") + " ore per one second")
   }, 33)
   
